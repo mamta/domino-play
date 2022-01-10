3648851125f39c2eadeee8f4cc379cdc25af0f1d
Mamta Bansal <mamta.bansal.mail@gmail.com>
2022-01-10 09:44:25 +0100

    Add sources

---
+++ src/main/java/com/mamta/play/dp/constants/AppConstants.java
@@ -0,0 +1,10 @@
+package com.mamta.play.dp.constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppConstants {
+
+    public static final List<String> paths= new ArrayList<>();
+
+}
